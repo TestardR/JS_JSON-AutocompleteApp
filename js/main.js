e9a193a162003c0ebcf22f1099a9cff664738e6e
unknown <rmtestard@gmail.com>
2019-04-29 17:44:17 +0200

    Get all states

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
+let states;
+
+// Get states
+const searchStates = async () => {
+  const res = await fetch('../data/states.json');
+  states = await res.json();
+};
+
+// Filter states
+const searchStates = searchText => {};
+
+search.addEventListener('input', () => searchStates(search.value));
