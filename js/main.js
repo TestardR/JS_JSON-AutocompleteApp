4e72ca0a12a81ba46f3f1137e4351f4a31bd0915
unknown <rmtestard@gmail.com>
2019-04-29 17:50:46 +0200

    filter states

--- js/main.js
+++ js/main.js
@@ -3,12 +3,20 @@ const matchList = document.getElementById('match-list');
 let states;
 
 // Get states
-const searchStates = async () => {
+const getStates = async () => {
   const res = await fetch('../data/states.json');
   states = await res.json();
 };
 
 // Filter states
-const searchStates = searchText => {};
+const searchStates = searchText => {
+  // Get matches to current text input
+  let matches = states.filter(state => {
+    const regex = new RegExp(`^${searchText}`, 'gi');
+    return state.name.match(regex) || state.abbr.match(regex);
+  });
+  console.log(matches);
+};
 
+window.addEventListener('DOMContentLoaded', getStates);
 search.addEventListener('input', () => searchStates(search.value));
